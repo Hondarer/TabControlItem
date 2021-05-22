a8f2578d68c99b912dda27b4abe40d707f04c0df
Tetsuo Honda <t-honda@hondarer-soft.com>
2021-05-22 14:15:04 +0900

    リファクタリング

--- TabControlItem/MainWindow.xaml.cs
+++ TabControlItem/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Text;
+﻿// 継承かビヘイビアか、どちらでもいけそう
+// DataContextがINotifyCollectionChangedの場合は考慮、ICollectionなら単発(ユースケースによって簡略化してもよいはず)
+// 左のDataCntexts、右のDataCntextsに分ける
+// 可視領域判定で見えないところを購読する
+// プロパティの変化を捕まえ集約する
+// 公開状態を更新する
+// テンプレートで状態をバインドする
+
+using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,18 +21,39 @@ namespace TabControlItem
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
 
             DataContext = new MainWindowViewModel();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            ScrollViewer scrollviewer = (ScrollViewer)tabControl.Template.FindName("scrollViewer", tabControl);
-            var children = ((Panel)scrollviewer.Content).Children;
+            Loaded -= MainWindow_Loaded;
 
+            ScrollViewer scrollviewer = (ScrollViewer)tabControl.Template.FindName("scrollViewer", tabControl);
+            DetermineVisible("MainWindow_Loaded", scrollviewer);
+            scrollviewer.DataContextChanged += Scrollviewer_DataContextChanged;
+            scrollviewer.ScrollChanged += Scrollviewer_ScrollChanged;
+        }
+
+        private void Scrollviewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // サイズが変わって表示されているアイテムの個数が変化した際にもきちんと呼ばれる
+            DetermineVisible("Scrollviewer_ScrollChanged", (ScrollViewer)sender);
+        }
+
+        private void Scrollviewer_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            DetermineVisible("Scrollviewer_DataContextChanged", (ScrollViewer)sender);
+        }
+
+        private void DetermineVisible(string reason, ScrollViewer scrollviewer)
+        {
             StringBuilder sb = new StringBuilder();
 
-            sb.AppendLine($"*** ScrollViewer ***");
+            sb.AppendLine($"{DateTime.Now} {reason}");
+
+            UIElementCollection children = ((Panel)scrollviewer.Content).Children;
 
             System.Drawing.Rectangle scrollviewerRectangle = new System.Drawing.Rectangle(
                 (int)scrollviewer.PointToScreen(new Point(0.0d, 0.0d)).X,
@@ -32,10 +62,11 @@ namespace TabControlItem
                 (int)scrollviewer.ActualHeight
                 );
 
-            sb.AppendLine($"  X={scrollviewerRectangle.X}");
-            sb.AppendLine($"  Y={scrollviewerRectangle.Y}");
-            sb.AppendLine($"  W={scrollviewerRectangle.Width}");
-            sb.AppendLine($"  H={scrollviewerRectangle.Height}");
+            //sb.AppendLine($"*** ScrollViewer ***");
+            //sb.AppendLine($"  X={scrollviewerRectangle.X}");
+            //sb.AppendLine($"  Y={scrollviewerRectangle.Y}");
+            //sb.AppendLine($"  W={scrollviewerRectangle.Width}");
+            //sb.AppendLine($"  H={scrollviewerRectangle.Height}");
 
             foreach (FrameworkElement child in children)
             {
@@ -47,10 +78,10 @@ namespace TabControlItem
                     );
 
                 sb.AppendLine($"*** Child ***");
-                sb.AppendLine($"    X={childRectangle.X}");
-                sb.AppendLine($"    Y={childRectangle.Y}");
-                sb.AppendLine($"    W={childRectangle.Width}");
-                sb.AppendLine($"    H={childRectangle.Height}");
+                //sb.AppendLine($"    X={childRectangle.X}");
+                //sb.AppendLine($"    Y={childRectangle.Y}");
+                //sb.AppendLine($"    W={childRectangle.Width}");
+                //sb.AppendLine($"    H={childRectangle.Height}");
                 sb.AppendLine($"    DataContext.Header={((ItemViewModel)child.DataContext).Header}");
                 sb.AppendLine($"    Contains={scrollviewerRectangle.Contains(childRectangle)}");
             }
